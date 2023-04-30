faeed0bcb57fcbb00359ad68bd6fae81ed825ad3
ijaz35 <ijaz.hossain1@gmail.com>
2023-04-30 16:26:26 +0600

    login register form added

--- src/pages/others/Login/Login.jsx
+++ src/pages/others/Login/Login.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import './Login.css'
 const Login = () => {
     return (
-        <div>
-            this is from login page
+        <div className='mx-auto w-50 '>
+            <h1 className='text-center'>Login</h1>
+            <form className='w-100 mx-auto'>
+
+                <label htmlFor="email">Username or Email</label>
+                <input className='d- w-100' type="text" name="email" id="email" required />
+                <label htmlFor="password">Password</label>
+                <input className='d- w-100' type="password" name="password" id="password" />
+
+                <input className='text-center w-100 mt-3 bg-primary' type="submit" value="submit" />
+            </form>
         </div>
     );
 };
--- src/pages/others/Register/Register.jsx
+++ src/pages/others/Register/Register.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 import './Register.css'
 const Register = () => {
     return (
-        <div>
-            <h1>This is register page</h1>
+        <div className='mx-auto w-50 '>
+            <h1 className='text-center'>Please Register</h1>
+            <form className='w-100 mx-auto'>
+                <label htmlFor="firstName">First Name</label>
+                <input className='d- w-100' type="text" name="firstName" id="firstName" required />
+                <label htmlFor="lastName">Last Name</label>
+                <input className='d- w-100' type="text" name="firstName" id="lastName" required />
+                <label htmlFor="email">Username or Email</label>
+                <input className='d- w-100' type="text" name="email" id="email" required />
+                <label htmlFor="password">Password</label>
+                <input className='d- w-100' type="password" name="password" id="password" />
+                <label htmlFor="confirmPassword">Confirm password</label>
+                <input className='d- w-100' type="password" name="confirmPassword" id="confirmPassword" required />
+                <input className='text-center w-100 mt-3 bg-primary' type="submit" value="submit" />
+            </form>
         </div>
     );
 };
